b5fba35d764afde309a8fc7e2a5605b164b3df5a
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-12 20:28:31 +0530

    Add DHT11 sensor reading functionality

---
+++ esp32_dht11.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
